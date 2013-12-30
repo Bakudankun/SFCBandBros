8dd21fea37fe2da7790d9cd008d113e4f84f9c29
Bakudankun <Bakudankun@users.noreply.github.com>
2013-12-30 17:19:00 +0900

    Buzzerクラス完成

--- SFCBandBros/Buzzer.h
+++ SFCBandBros/Buzzer.h
@@ -1,31 +1,28 @@
 /* 
- * キー番号からtone関数で音を鳴らすための関数群。
+ * Buzzerクラス。MIDIっぽい命令でブザーを鳴らす。
  */
 
 #include "Arduino.h"
 
-#define NOTE_A4  440
-#define BUZZER 6
+class Buzzer{
+	public:
+		Buzzer(byte pin);
+		// コンストラクタ。ピン番号pinのブザーを起動する。
+		// pin:ブザーを繋いだピン番号
+		
+		void noteOn(char key);
+		// ノート番号keyの音を鳴らす。
 
-unsigned int ktof(byte key){
-	// キー番号からtone関数で使うための周波数をunsigned intで返す。
-	// key: キー番号。MIDIに従い、A4が81。
-	// return: 周波数。tone関数の引数がunsigned intのため四捨五入して返す。
-	//         A4が440Hzだが、10Hzを下回らない。
+		void noteOff();
+		// 今鳴っている音を止める。
+	
+	private:
+		unsigned int ktof(char key);
+		// キー番号からtone関数で使うための周波数をunsigned intで返す。
+		// key: キー番号。MIDIに従い、A4が81。
+		// return: 周波数。tone関数の引数がunsigned intのため四捨五入して返す。
+		//         A4が440Hzだが、10Hzを下回らない。
+		
+		byte pin; // ブザー出力のピン番号
 
-	unsigned int f = (unsigned int)(440 * pow(2, ((float)(key - 81))/12.0) + 0.5);
-	return max(10, f);
 }
-
-void noteOn(byte key){
-	tone(BUZZER, ktof(key));
-	return;
-}
-
-void noteOff(){
-	noTone(BUZZER);
-	return;
-}
-
-
-
